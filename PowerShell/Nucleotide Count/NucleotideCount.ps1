b4618808cda8f7e30e1094bfc0361267faf9a647
RSCyberTech <ricardo.ams@hotmail.com>
2024-02-16 11:18:23 +0000

    Create NucleotideCount.ps1

---
+++ PowerShell/Nucleotide Count/NucleotideCount.ps1
@@ -0,0 +1,43 @@
+Function Get-NucleotideCount() {
+    <#
+    .SYNOPSIS
+    Given a single stranded DNA string, compute how many times each nucleotide occurs in the string.
+    
+    .DESCRIPTION
+    The genetic language of every living thing on the planet is DNA.
+    DNA is a large molecule that is built from an extremely long sequence of individual elements called nucleotides.
+    4 types exist in DNA and these differ only slightly and can be represented as the following symbols: 'A' for adenine, 'C' for cytosine, 'G' for guanine, and 'T' thymine.
+
+    The function counts the occurances of A, C, G and T in the supplied strand.
+    And returns a hashtable in the format:
+
+    @{ A = 2; C = 2; G = 2; T = 3 }
+    
+    .PARAMETER Strand
+    The DNA strand to count
+    
+    .EXAMPLE
+    Get-NucleotideCount -Strand "ACGTAGCTT"
+    
+    Returns: @{ A = 2; C = 2; G = 2; T = 3 }
+    #>
+    [CmdletBinding()]
+    Param(
+        [string]$Strand
+    )
+
+    $numbers = @{ A = 0; C = 0; G = 0; T = 0 }
+
+    if ($strand.Length -gt 0) {
+        foreach ($letter in $Strand.ToCharArray()) {
+            if ($letter -iin $numbers.Keys) {
+                $numbers[[string]$letter] += 1
+            }
+            else {
+                throw "Invalid nucleotide in strand"
+            }
+        }
+        return $numbers
+    }
+    return $numbers
+}
