1e2fb0d4445820e9991f598d85dd4dff51e2dae8
RSCyberTech <ricardo.ams@hotmail.com>
2024-05-01 16:01:31 +0100

    Update darts.sql

--- SQLite/Darts/darts.sql
+++ SQLite/Darts/darts.sql
@@ -1,4 +1,7 @@
 -- Schema: CREATE TABLE "darts" ("x" REAL, "y" REAL, score INTEGER);
 -- Task: update the darts table and set the score based on the x and y values.
 
-update darts set 
+update darts set score=10 where (sqrt(power(abs(x),2)+power(abs(y),2)))<=1;
+update darts set score=5 where (sqrt(power(abs(x),2)+power(abs(y),2)))<=5 and (sqrt(power(abs(x),2)+power(abs(y),2)))>1;
+update darts set score=1 where (sqrt(power(abs(x),2)+power(abs(y),2)))<=10 and (sqrt(power(abs(x),2)+power(abs(y),2)))>5;
+update darts set score=0 where (sqrt(power(abs(x),2)+power(abs(y),2)))>10;
